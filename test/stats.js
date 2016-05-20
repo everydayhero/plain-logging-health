01b234536a188328e3937f9fecca7f57399b2aca
Keiran <keiran.oleary@everydayhero.com.au>
2016-05-20 14:02:13 +1000

    Update env app name

--- test/stats.js
+++ test/stats.js
@@ -7,16 +7,16 @@ describe('Stats adaptor', () => {
     stats.increment('test_increment')
     stats.decrement('test_decrement.with bad% char@ct&rs')
     stats.gauge('test_gauge', 400)
-    expect(stats._getQueue()).to.contain('plain_logging_health.test_increment:1|c\n')
-    expect(stats._getQueue()).to.contain('plain_logging_health.test_decrement.with_bad__char_ct_rs:-1|c\n')
-    expect(stats._getQueue()).to.contain('plain_logging_health.test_gauge:400|g\n')
+    expect(stats._getQueue()).to.contain('plain-logging-health.test_increment:1|c\n')
+    expect(stats._getQueue()).to.contain('plain-logging-health.test_decrement.with_bad__char_ct_rs:-1|c\n')
+    expect(stats._getQueue()).to.contain('plain-logging-health.test_gauge:400|g\n')
   })
 
   it('tracks timers', (done) => {
     stats.timerStart('test_timer')
     setTimeout(() => {
       stats.timerStop('test_timer')
-      expect(stats._getQueue()).to.contain('plain_logging_health.test_timer:')
+      expect(stats._getQueue()).to.contain('plain-logging-health.test_timer:')
       done()
     }, 5)
   })
